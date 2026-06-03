6b9179cd175494098cc0e5e038d4bfcdadf42cbe
ManuelDenis <popescu.manuel.denis@gmail.com>
2026-06-03 10:33:28 +0300

    update main.py5

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ if not TWILIO_AUTH_TOKEN:
 validator = RequestValidator(TWILIO_AUTH_TOKEN)
 
 ALLOWED_NUMBERS = set(os.getenv("ALLOWED_NUMBERS", "").split(","))
-MAX_SILENCE_SECONDS = 60
+MAX_SILENCE_SECONDS = 90
 
 # Configuration
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')  # requires OpenAI Realtime API Access
